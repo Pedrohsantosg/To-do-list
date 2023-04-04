1e11f417ea9e36fc6826f2f4e5804cbe3621d6b2
Pedro Henrique Santos <pedro.henrique.gomes@maisunifacisa.com.br>
2023-04-04 16:45:46 -0300

    colocando uma img quando não tem nada na lista

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,25 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-//Componentes
-import { ComponentsComponent } from './components/components.component';
-import { HeaderComponent } from './components/header/header.component';
-import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
-import { TodoInputAddItensComponent } from './components/todo-input-add-itens/todo-input-add-itens.component';
-import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { FormsModule } from '@angular/forms';
 
 //Pages
 import { HomeComponent } from './pages/home/home.component';
 
+//Components
+import { HeaderComponent } from './components/header/header.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { TodoInputAddItensComponent } from './components/todo-input-add-itens/todo-input-add-itens.component';
+import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
+
 @NgModule({
   declarations: [
-    ComponentsComponent,
-    TodoButtonDeleteAllComponent,
-    TodoInputAddItensComponent,
-    TodoListComponent,
     HomeComponent,
     HeaderComponent,
+    TodoListComponent,
+    TodoInputAddItensComponent,
+    TodoButtonDeleteAllComponent,
   ],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
 })
 export class HomeModule {}
