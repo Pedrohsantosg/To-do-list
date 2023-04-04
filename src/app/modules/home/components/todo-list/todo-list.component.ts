3340b4e192d23c4d1635fcf56da81fdbf3187cbc
Pedro Henrique Santos <pedro.henrique.gomes@maisunifacisa.com.br>
2023-04-04 16:57:34 -0300

    atualizando o botão de deletar todos

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -9,9 +9,20 @@ import { TaskList } from '../../model/task-list';
   styleUrls: ['./todo-list.component.scss'],
 })
 export class TodoListComponent implements OnInit {
-  public taskList: Array<TaskList> = [];
+  public taskList: Array<TaskList> = [{ task: 'meu pedido', checked: true }];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  public deleteItemTaskList(event: number) {
+    this.taskList.splice(event, 1);
+  }
+
+  public deleteAllTaskList() {
+    const confirm = window.confirm('Deseja deletar todas?');
+    if (confirm) {
+      this.taskList = [];
+    }
+  }
 }
